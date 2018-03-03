202108fb000343daab59d6b749508117b01723f7
cpreid <cpreid>
2018-03-02 20:24:56 -0500

    updating extension to use new api

--- analytics-integration.js
+++ analytics-integration.js
@@ -1,6 +1,22 @@
-if(extension.listen_for_evt && extension.track_ttc_event) {
-  window.optimizely.get("custom/ttc").listenTrack(
-    extension.listen_for_evt, 
-    extension.track_ttc_event, 
-    extension.resettable === 'yes');
+if(extension.start_at_evt && extension.stop_at_evt && extension.elapsed_ttc_metric) {
+  var ttc = window.optimizely.get("custom/ttc"),
+      configOptions = {resettable:false};
+  
+  // configure resettable parameter
+    var resettableThreshold = parseFloat(extension.resettable_threshold),
+      resettable = extension.resettable === 'yes';
+  configOptions.resettable = resettableThreshold || resettable;
+  
+  // configure min/max
+  var min = parseFloat(extension.max_ttc),
+      max = parseFloat(extension.min_ttc);
+  if(min) configOptions.min = min;
+  if(max) configOptions.max = max;  
+  
+    ttc.bindTTCTracking(
+    extension.start_at_evt, 
+    extension.stop_at_evt, 
+        extension.elapsed_ttc_metric, 
+    configOptions
+  );
 }
